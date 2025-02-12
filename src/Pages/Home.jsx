f1ef9b11aabc397e838c0f13d33c88087ae89f13
Muhammad Mubashir <m.mubashirweb@gmail.com>
2025-02-12 12:58:40 +0500

    the content

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -245,7 +245,7 @@ function Home() {
         trigger: page.current,
         scroller: "body",
         start: "top -2%", // Adjust the start position
-        end: "+=2000", // Increase the end value to add a delay
+        end: "+=2500", // Increase the end value to add a delay
         scrub: 2,
         pin: true,
         markers: true, // Shows markers for debugging
@@ -313,7 +313,7 @@ function Home() {
   return (
     <>
       <section className="min-h-screen">
-        <div className="relative h-screen w-full bg-black overflow-hidden">
+        <div className="relative h-screen w-full bg-black ">
           {/* Navbar */}
           <div ref={navbarRef} className="relative z-10">
             <Navbar />
@@ -371,13 +371,13 @@ function Home() {
       </section>
 
       {/* Horizontal Scroll Section */}
-      <section ref={page} className="overflow-hidden">
+      <section ref={page} className="overflow-x-auto">
         <div
           ref={page2H2}
           className="flex w-[300vw] min-h-screen  py-6 md:py-0"
         >
           <div className="h-screen bg-white grid  text-center md:text-start md:gap-20  md:grid-cols-2  px-4 items-center justify-center max-w-[1440px] mx-auto w-[90%]">
-            <div className="flex flex-col items-center">
+            <div className="flex flex-col items-center md:items-start">
               <h1 className="lg:text-5xl md:text-4xl text-3xl text-primary-light py-6 font-semibold">
                 Are You Busy
               </h1>
